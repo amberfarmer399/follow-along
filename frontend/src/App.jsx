c0cabd966e2a05f9ca6995d1bbe33c8cf99455f3
Prithvi Singh <prithvisingh@Prithvis-MacBook-Air.local>
2025-02-04 14:38:14 +0530

    completed the assignment and added to the readme file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"; 
-import { LoginPage, SignupPage, Home } from "./Routes.jsx"
+import { LoginPage, SignupPage, Home,CreateProduct} from "./Routes.jsx"
 
 import './App.css';
+import CreateProduct from "./pages/createProduct.jsx";
 
 function App(){
   return(
@@ -10,6 +11,7 @@ function App(){
         <Route path="/Login" element={<LoginPage/>}/>
         <Route path="/Signup" element={<SignupPage/>}/>
         <Route path="/Home" element={<Home/>}/>
+        <Route path="/create-product" element={<CreateProduct />} />
 
       </Routes>
     </BrowserRouter>
