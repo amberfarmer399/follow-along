02602790b15240828c639476bb05278b951fcd4a
Prithvi Singh <prithvisingh@Prithvis-MacBook-Air.local>
2025-02-18 15:14:48 +0530

    done milestone 19 and 20

--- frontend/src/pages/myProducts.jsx
+++ frontend/src/pages/myProducts.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import Myproduct from "../components/auth/myproduct";
+
+import Myproduct from "../components/auth/myProduct";
 import NavBar from "../components/auth/nav";
 
 export default function MyProducts() {
